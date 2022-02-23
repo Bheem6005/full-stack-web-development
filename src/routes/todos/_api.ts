af44ace647faf8c4e73b84e273313b0eb51d9d3e
Bheem Suttipong <bheem.su@ku.th>
2022-02-23 14:54:51 +0000

    Add PAtch endpoint - update text

--- src/routes/todos/_api.ts
+++ src/routes/todos/_api.ts
@@ -1,7 +1,9 @@
+import type { Request } from "@sveltejs/kit";
+
 // TODO: Persist in database
 let todos: Todo[] = [];
 
-export const api = (request: Request, todo?: Todo) => {
+export const api = (request: Request, data?: Record<string, unknown>) => {
   let body = {};
   let status = 500;
 
@@ -11,12 +13,22 @@ export const api = (request: Request, todo?: Todo) => {
       status = 200;
       break;
     case "POST":
-      todos.push(todo);
-      body = todo;
+      todos.push(data as Todo);
+      body = data;
       status = 201;
       break;
     case "DELETE":
       todos = todos.filter(todo => todo.uid !== request.params.uid)
+      status = 200;
+      break;
+
+    case "PATCH":
+      todos = todos.map(todo => {
+        if (todo.uid === request.params.uid) {
+          todo.text = data.text as string;
+        }
+        return todo;
+      });
       status = 200;
       break;
 
